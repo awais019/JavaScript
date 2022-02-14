bd6c28586579b98a497ea448d81fc80e530444b1
Muhammad Awais <75579534+Awais-019@users.noreply.github.com>
2022-02-14 12:18:42 +0500

    Update index.js

--- Webpack Boilerplate/src/index.js
+++ Webpack Boilerplate/src/index.js
@@ -1,12 +1,1 @@
-import {styleBody, addTitle, contact} from './dom';
-import users, { getPremiumUsers } from './data';
 
-console.log('index file');
-addTitle('test');
-styleBody();
-console.log(contact);
-
-const premiumUsers = getPremiumUsers(users);
-console.log(users, premiumUsers);
-
-console.log('test');
